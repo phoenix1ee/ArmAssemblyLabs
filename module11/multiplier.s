f3f59cc2f58821c92afa7e214ad1433a54f3e02b
morpheus <110279025+phoenix1ee@users.noreply.github.com>
2025-04-10 22:07:28 -0700

    updated multiplier.s

--- module11/multiplier.s
+++ module11/multiplier.s
@@ -14,33 +14,92 @@
 main:
 
     #program library:
-    #r4: 
-    #r5: 
-    #r6: 
+    #r4: sign of r0, 1 denote +ve, 0 denote -ve
+    #r5: sign of r1, 1 denote +ve, 0 denote -ve
         
     #push stack
     SUB sp, sp, #4
     STR lr, [sp, #0] 
 
+    LDR r0, =promptin
+    BL printf
     
+    LDR r0, =format1
+    LDr r1, =int1
+    BL scanf
+
+    LDR r0, =format1
+    LDr r1, =int2
+    BL scanf
+
+    LDR r0, =int1
+    LDR r0, [r0]
+
+    #check the sign of integer1 or if int1=0
+    MOV r2, #0
+    CMP r0, r2
+    BEQ zero
+    SUBLT r0, r2, r0
+    MOVLT r4, #0
+    MOVGT r4, #1
+
+    LDR r1, =int2
+    LDR r1, [r1]
+
+    #check the sign of integer2 or if int2=0
+    MOV r2, #0
+    CMP r1, r2
+    BEQ zero
+    SUBLT r1, r2, r1
+    MOVLT r5, #0
+    MOVGT r5, #1
+    
+    #call the multiply function
+    BL product
+    #after return, output results with correct sign
+    CMP r4, r5
+    BEQ positve
+    BNE negative
+    
+    #if int1/int2 = 0, return 0
+    zero:
+    MOV r1, #0
+    B endmain
+
+    #if int1 and int2 have same sign, i.e. +ve
+    positve:
+    MOV r1, r0
+    B endmain
+
+    #if int1 and int2 have different sign, i.e. -ve
+    negative:
+    MOV r2, #0
+    SUB r1, r2, r0
+    B endmain
+
+    #printout products with correct sign
+    endmain:
+    LDR r0, =output
+    BL printf
+
     #pop stack
     LDR lr, [sp, #0]
-    ADD sp, sp, #8
+    ADD sp, sp, #4
     MOV pc, lr
 .data
-    promptCont: .asciz "\n : "
-    promptError: .asciz "\n\n\n"
+    promptin: .asciz "Please input two integers\n : "
 
     format1: .asciz "%d"
-    input: .word 0
 
-    outputYes: .asciz "\n\n\n"
-    outputNo: .asciz "\n\n\n"
+    int1: .word 0
+    int2: .word 0
+
+    output: .asciz "The product of the two number is %d.\n"
 
 #End main
 
 .text
-multiplier: 
+product: 
     # A recursive function to multiply two numbers. Take 2 input: r0:integer x r1:integer y
     # r0 = r0+r0+...+r0 for r1 times 
     # return at r0 the sum
@@ -54,14 +113,13 @@ multiplier:
     MOV r4, r0
     MOV r5, r1
 
-    #if y=0, return
-    CMP r5, #0
-    B return
+    #if y=1, return
+    CMP r1, #1
+    BEQ return
 
     #if y>0, y=y-1
-    SUB r5, r5, #1
-    MOV r1, r5
-    BL multiplier
+    SUB r1, r1, #1
+    BL product
     ADD r0, r0, r4
     
     return:
@@ -72,37 +130,5 @@ multiplier:
     ADD sp, sp, #12
     MOV pc, lr
 .data
-#End multiplier
+#End product
 
-
-.text
-findremainder: 
-    # A function find reminder. Take 2 input: r0:dividend r1:divisor
-    # return the divisor back at r0
-    # return the remainder at r1
-    
-    #push stack
-    SUB sp, sp, #12
-    STR lr, [sp, #0]
-    
-    # store the r0:dividend
-    STR r0, [sp, #4]
-    # store r1:divisor
-    STR r1, [sp, #8]
-    
-    #function core    
-    #find the quotient
-    BL __aeabi_idiv
-    MOV r1, r0
-    LDR r0, [sp, #8]
-    LDR r2, [sp, #4]
-    MUL r1, r0, r1
-    SUB r1, r2, r1
-        
-    end:
-    #pop stack
-    LDR lr, [sp, #0]
-    ADD sp, sp, #12
-    MOV pc, lr
-.data
-#End findremainder
